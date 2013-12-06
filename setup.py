c8650e27807ee0e49ff8a5e8448b008fe6268efd
Atsuo Ishimoto <ishimoto@gembook.org>
2013-12-06 09:58:07 +0900

    Initial import.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os, sys
+from setuptools import setup, find_packages, Extension
+
+def read(fname):
+    return open(
+            os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "kaadbg",
+    version = "0.1.0",
+    description='Remote debugger runner for kaa',
+    url='https://github.com/kaaedit/kaadbg',
+    author='Atsuo Ishimoto',
+    author_email='ishimoto@gembook.org',
+    long_description=read('README.rst'),
+    classifiers=[
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Development Status :: 3 - Alpha",
+            "Topic :: Text Editors",
+            "Topic :: Software Development :: Debuggers",
+            "Environment :: Console :: Curses",
+            "License :: OSI Approved :: MIT License", ],
+    license='MIT License',
+    packages = find_packages(),
+)
